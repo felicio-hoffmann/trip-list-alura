0bbdf901bdb1b62655ba3f682d22aec1d69b3578
Felicio Hoffmann <felicio.pereira@lsitec.org.br>
2022-03-28 11:21:06 -0300

    Adicionando as cores e ajustando o divisor do ListView. Começo da implementação de novas telas

---
+++ app/src/main/java/com/example/viagens/ui/activity/TripSummaryActivity.java
@@ -0,0 +1,16 @@
+package com.example.viagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.viagens.R;
+
+public class TripSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_summary);
+    }
+}
